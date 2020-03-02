5cce290067ddb84816bbce4eb706a384f8c06f07
tpvt96 <tranphong96.hbk@gmail.com>
2020-03-02 10:31:06 +0800

    finish 2.5

--- Chapter 2/2_5/main.c
+++ Chapter 2/2_5/main.c
@@ -3,6 +3,10 @@
 
 int main()
 {
-    printf("Hello world!\n");
+    printf("Enter a value: ");
+    float x;
+    scanf("%f", &x);
+    printf("Value of polynomial: 3x^5+2x^4-5x^3-x^2+7x-6: %.0f", 3*x*x*x*x*x+
+           2*x*x*x*x - 5*x*x*x - x*x + 7*x -6);
     return 0;
 }
