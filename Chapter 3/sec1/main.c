f21fed1ff25d4db0d2636343eb71f012bef16eae
tpvt96 <tranphong96.hbk@gmail.com>
2020-03-03 13:49:22 +0800

    add sec3_1

---
+++ Chapter 3/sec1/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("%6d, %4d\n", 86, 1040);
+    printf("%12.5e\n", 30.253);
+    printf("%.4f\n", 83.162);
+    printf("%6.3g\n", .0000009979);
+    printf("%10.3g\n", 839.21f);
+    return 0;
+}
