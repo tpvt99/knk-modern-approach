60c2967e98c8457e2cfea2785ba299b5eead4cc9
tpvt96 <tranphong96.hbk@gmail.com>
2020-03-04 17:58:21 +0800

    add sec2-4

---
+++ Chapter 3/sec2/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    float x = 123.9382;
+    printf("%-8.1e|%10.6e|%-8.3f|%6.0f", x,x,x,x);
+
+    return 0;
+}
---
+++ Chapter 3/sec3/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    //int x;
+    //scanf("%d", &x);
+    //scanf(" %d", &x);
+    //printf("%d", x);
+    //int x,y,z;
+    //scanf("%d-%d-%d", &x, &y, &z);
+    //scanf("%d -%d -%d", &x, &y, &z);
+    //printf("%d %d %d\n", x, y, z);
+    float f;
+    scanf("%f ", &f);
+    printf("%f\n", f);
+
+    return 0;
+}
