3e6e843441710b5346658621af09b566eb2a6313
tpvt96 <tranphong96.hbk@gmail.com>
2020-03-06 17:58:31 +0800

    add p6

--- Chapter 3/p5/main.c
+++ Chapter 3/p5/main.c
@@ -13,10 +13,10 @@ int main()
     scanf("%d%d%d%d", &c1, &c2, &c3, &c4);
     scanf("%d%d%d%d", &d1, &d2, &d3, &d4);
 
-    printf("%d %d %d %d\n", a1, a2, a3, a4);
-    printf("%d %d %d %d\n", b1, b2, b3, b4);
-    printf("%d %d %d %d\n", c1, c2, c3, c4);
-    printf("%d %d %d %d\n", d1, d2, d3, d4);
+    printf("%2d %2d %2d %2d\n", a1, a2, a3, a4);
+    printf("%2d %2d %2d %2d\n", b1, b2, b3, b4);
+    printf("%2d %2d %2d %2d\n", c1, c2, c3, c4);
+    printf("%2d %2d %2d %2d\n", d1, d2, d3, d4);
 
     printf("Row sums: %d %d %d %d\n", a1+a2+a3+a4, b1+b2+b3+b4, c1+c2+c3+c4, d1+d2+d3+d4);
     printf("Column sums: %d %d %d %d\n",a1+b1+c1+d1, a2+b2+c2+d2, a3+b3+c3+d3, a4+b4+c4+d4);
