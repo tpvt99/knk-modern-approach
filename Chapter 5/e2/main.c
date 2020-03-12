81da480fae03ada7585a86f8237273e220acc2b5
tpvt96 <tranphong96.hbk@gmail.com>
2020-03-12 13:55:23 +0800

    chap5 e2

---
+++ Chapter 5/e2/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int i, j, k;
+    i = 10; j = 5;
+    printf("%d\n", !i < j);
+    i = 2; j = 1;
+    printf("%d\n", !!i + !j);
+    i = 5; j = 0; k = -5;
+    printf("%d\n", i && j || k);
+    i = 1; j = 2; k = 3;
+    printf("%d\n", i < j || k);
+    return 0;
+}
