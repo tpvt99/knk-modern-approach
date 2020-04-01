ae71ae816c84d3c71256389294ed5d82933bf177
tpvt96 <tranphong96.hbk@gmail.com>
2020-04-01 10:20:48 +0800

    add p3

---
+++ Chapter 8/p3/main.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+int main()
+{
+    int digit;
+    long int n;
+    printf("Enter a number: ");
+    scanf("%ld", &n);
+    while (n != 0) {
+        bool digit_seen = false;
+        int digit_counts[10] = {0};
+        while (n > 0) {
+            digit = n % 10;
+            if (digit_counts[digit])
+                digit_seen = true;
+            digit_counts[digit] += 1;
+            n /= 10;
+        }
+        if (digit_seen) {
+            printf("Repeated digit(s): ");
+            for (int i = 0; i < 10; i++)
+                if (digit_counts[i] >= 2)
+                    printf("%d ", i);
+        }
+        else
+            printf("No repeated digit\n");
+        printf("\nEnter a number: ");
+        scanf("%ld", &n);
+    }
+
+    return 0;
+}
