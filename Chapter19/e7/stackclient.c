f93c4d71d290ba28e47d02ee0dbd2f7ddd379c4b
tpvt96 <tranphong96.hbk@gmail.com>
2020-06-09 16:35:27 +0800

    finish all exercises chap 19

---
+++ Chapter19/e7/stackclient.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include "stackADT.h"
+
+int main(void) {
+    Stack s1, s2;
+    int n;
+
+    s1 = create(2);
+    s2 = create(2);
+
+    push(s1, 1);
+    push(s1, 2);
+    push(s1, 5);
+    push(s1, 10);
+    push(s1, 20);
+    printf("Peek of s1: %d\n", peek(s1));
+    n = pop(s1);
+    printf("Popped %d from s1\n", n);
+    push(s2, n);
+    n = pop(s1);
+    printf("Popped %d from s1\n", n);
+    push(s2, n);
+
+    printf("Peek of s2: %d\n", peek(s2));
+
+    destroy(s1);
+    while (!is_empty(s2))
+        printf("Popped %d from s2\n", pop(s2));
+
+    push(s2, 3);
+    make_empty(s2);
+    if (is_empty(s2))
+        printf("S2 is empty\n");
+    else
+        printf("S2 is not empty\n");
+    destroy(s2);
+    return 0;
+}
+
